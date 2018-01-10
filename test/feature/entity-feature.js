7293143da1c0d718302332de1908d4928595e927
Markus Ekholm <markus.ekholm@bonniernews.se>
2018-01-10 13:58:30 +0100

    Added tests for the soft remove feature
    * Without force a list/read operations should not yield a result.
    * With force list/read operations should work.
    * Upserting a soft removed entity should not work.

--- test/feature/entity-feature.js
+++ test/feature/entity-feature.js
@@ -16,13 +16,13 @@ Feature("Entity", () => {
     }
   };
 
-  let savedEntity;
-
-  before((done) => {
-    helper.clearAndInit(done);
-  });
-
   Scenario("Save and load an entity", () => {
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
+
+    let savedEntity;
 
     Given("a new entity is saved", (done) => {
       crud.upsert(entity, done);
@@ -30,9 +30,9 @@ Feature("Entity", () => {
 
     When("we load it", (done) => {
       crud.load(entity.id, (err, dbEntity) => {
-        if (err) done(err);
+        if (err) return done(err);
         savedEntity = dbEntity;
-        done();
+        return done();
       });
     });
 
@@ -40,6 +40,88 @@ Feature("Entity", () => {
       savedEntity.id.should.equal(entity.id);
       savedEntity.type.should.equal(entity.type);
       savedEntity.attributes.name.should.equal(entity.attributes.name);
+    });
+  });
+
+  Scenario("Save, remove and try to load", () => {
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
+
+    Given("a new entity is saved", (done) => {
+      crud.upsert(entity, done);
+    });
+
+    When("we delete it", (done) => {
+      crud.remove(entity.id, done);
+    });
+
+    Then("it should not be possible to load without force", (done) => {
+      crud.load(entity.id, (err, dbEntity) => {
+        if (err) return done(err);
+        should.equal(dbEntity, null);
+        return done();
+      });
+    });
+  });
+
+  Scenario("Save, remove and forcefully load an entity", () => {
+    let savedEntity;
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
+
+    Given("a new entity is saved", (done) => {
+      crud.upsert(entity, done);
+    });
+
+    When("we delete it", (done) => {
+      crud.remove(entity.id, (err) => {
+        if (err) return done(err);
+        return done();
+      });
+    });
+
+    Then("we load it with force", (done) => {
+      crud.load(entity.id, true, (err, dbEntity) => {
+        if (err) return done(err);
+        savedEntity = dbEntity;
+        return done();
+      });
+    });
+
+    And("it should have the same data", () => {
+      savedEntity.id.should.equal(entity.id);
+      savedEntity.type.should.equal(entity.type);
+      savedEntity.attributes.name.should.equal(entity.attributes.name);
+    });
+  });
+
+  Scenario("Save, remove and try to update an entity", () => {
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
+
+    Given("a new entity is saved", (done) => {
+      crud.upsert(entity, done);
+    });
+
+    And("we delete it", (done) => {
+      crud.remove(entity.id, (err) => {
+        if (err) return done(err);
+        return done();
+      });
+    });
+
+    Then("it should not be possible to update the entity", (done) => {
+      crud.upsert(entity, (err, res) => {
+        should.not.equal(err, null);
+        should.equal(res, null);
+        done();
+      });
     });
   });
 });
--- test/feature/version-feature.js
+++ test/feature/version-feature.js
@@ -19,16 +19,17 @@ Feature("Version", () => {
     type: "person"
   };
 
-  let entityVersions;
-  let versionNrTwoId;
-  let versionNrTwo;
   const correlationIds = ["x", "y"];
 
-  before((done) => {
-    helper.clearAndInit(done);
-  });
-
   Scenario("Save and load multiple versions of an entity", () => {
+
+    let entityVersions;
+    let versionNrTwoId;
+    let versionNrTwo;
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
 
     Given("a new entity is saved", (done) => {
       entity.attributes = attributes[0];
@@ -78,4 +79,123 @@ Feature("Version", () => {
       versionNrTwo.correlationId.should.eql(correlationIds[0]);
     });
   });
+
+  Scenario("Save multiple versions of an entity, remove the entity and try to list and load the versions", () => {
+
+    let versionNr;
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
+
+    Given("a new entity is saved", (done) => {
+      entity.attributes = attributes[0];
+      crud.upsert(entity, done);
+    });
+
+    And("a new version is added to the entity", (done) => {
+      entity.attributes = attributes[1];
+      entity.meta = { correlationId: correlationIds[0] };
+      crud.upsert(entity, done);
+    });
+
+    And("another version is added", (done) => {
+      entity.attributes = attributes[2];
+      entity.meta = { correlationId: correlationIds[1] };
+      crud.upsert(entity, done);
+    });
+
+    And("we note a specific version id", (done) => {
+      crud.listVersions(entity.id, (err, dbEntity) => {
+        if (err) done(err);
+        versionNr = dbEntity[1];
+        done();
+      });
+    });
+
+    When("we remove the entity", (done) => {
+      crud.remove(entity.id, done);
+    });
+
+    Then("getting all the versions should fail", (done) => {
+      crud.listVersions(entity.id, (err, dbEntity) => {
+        should.not.equal(err, null);
+        should.equal(dbEntity, null);
+        done();
+      });
+    });
+
+    And("getting a specific version should fail", (done) => {
+      crud.loadVersion(versionNr, (err, dbEntity) => {
+        should.not.equal(err, null);
+        should.equal(dbEntity, null);
+        done();
+      });
+    });
+  });
+
+
+  Scenario("Save, remove and forcefully load multiple versions of an entity", () => {
+    let entityVersions;
+    let versionNrTwoId;
+    let versionNrTwo;
+
+    before((done) => {
+      helper.clearAndInit(done);
+    });
+
+    Given("a new entity is saved", (done) => {
+      entity.attributes = attributes[0];
+      crud.upsert(entity, done);
+    });
+
+    And("a new version is added to the entity", (done) => {
+      entity.attributes = attributes[1];
+      entity.meta = { correlationId: correlationIds[0] };
+      crud.upsert(entity, done);
+    });
+
+    And("another version is added", (done) => {
+      entity.attributes = attributes[2];
+      entity.meta = { correlationId: correlationIds[1] };
+      crud.upsert(entity, done);
+    });
+
+    When("we remove the entity", (done) => {
+      crud.remove(entity.id, done);
+    });
+
+    When("we forcefully get all the versions", (done) => {
+      crud.listVersions(entity.id, true, (err, dbEntity) => {
+        if (err) done(err);
+        entityVersions = dbEntity;
+        done();
+      });
+    });
+
+    Then("it should return all added versions", () => {
+      entityVersions.should.have.lengthOf(3);
+      entityVersions[1].correlationId.should.equal(correlationIds[0]);
+    });
+
+    Given("the returned version id of the second version", () => {
+      versionNrTwoId = entityVersions[1].versionId;
+    });
+
+    When("we forcefully fetch the second version", (done) => {
+      crud.loadVersion(versionNrTwoId, true, (err, dbEntity) => {
+        if (err) done(err);
+        versionNrTwo = dbEntity;
+        done();
+      });
+    });
+
+    Then("it should have the attributes that was saved in that version", () => {
+      versionNrTwo.versionId.should.equal(versionNrTwoId);
+      versionNrTwo.entity.attributes.should.eql(attributes[1]);
+      versionNrTwo.correlationId.should.eql(correlationIds[0]);
+    });
+  });
+
+
 });
